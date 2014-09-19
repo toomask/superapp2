ac8e6c71dac7e4ea63afe38aec8c414254492c69
toomas <toomas@mooncascade.com>
2014-09-19 11:04:57 +0300

    c

--- app/src/main/java/com/mooncascade/superapp2/GcmIntentService.java
+++ app/src/main/java/com/mooncascade/superapp2/GcmIntentService.java
@@ -2,8 +2,10 @@ package com.mooncascade.superapp2;
 
 import android.app.IntentService;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
@@ -44,11 +46,11 @@ public class GcmIntentService extends IntentService {
              */
             if (GoogleCloudMessaging.
                     MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-                sendNotification("Send error: " + extras.toString());
+                sendNotification("Send error: " + extras.toString(), null);
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_DELETED.equals(messageType)) {
                 sendNotification("Deleted messages on server: " +
-                        extras.toString());
+                        extras.toString(), null);
                 // If it's a regular GCM message, do some work.
             } else if (GoogleCloudMessaging.
                     MESSAGE_TYPE_MESSAGE.equals(messageType)) {
@@ -63,7 +65,7 @@ public class GcmIntentService extends IntentService {
                 }
                 Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
                 // Post notification of received message.
-                sendNotification("Received: " + extras.toString());
+                sendNotification("Received: " + extras.toString(), extras.getString("url"));
                 Log.i(TAG, "Received: " + extras.toString());
             }
         }
@@ -74,12 +76,9 @@ public class GcmIntentService extends IntentService {
     // Put the message into a notification and post it.
     // This is just one simple example of what you might choose to do with
     // a GCM message.
-    private void sendNotification(String msg) {
+    private void sendNotification(String msg, String url) {
         mNotificationManager = (NotificationManager)
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-//                new Intent(this, DemoActivity.class), 0);
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
@@ -89,7 +88,13 @@ public class GcmIntentService extends IntentService {
                                 .bigText(msg))
                         .setContentText(msg);
 
-//        mBuilder.setContentIntent(contentIntent);
+        if (url != null) {
+            Intent notificationIntent = new Intent(Intent.ACTION_VIEW);
+            notificationIntent.setData(Uri.parse(url));
+            PendingIntent contentIntent = PendingIntent.getActivity(getBaseContext(), 0, notificationIntent, 0);
+            mBuilder.setContentIntent(contentIntent);
+        }
+
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
 
         Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
